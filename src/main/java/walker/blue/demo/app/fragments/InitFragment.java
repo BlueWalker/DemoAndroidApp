de4d32cba5ad53725bb7fe7864b1fd6f189a34ec
acompagno <acompag@gmail.com>
2015-03-09 20:30:31 -0400

    Added permission and updated InitFragment

--- src/main/java/walker/blue/demo/app/fragments/InitFragment.java
+++ src/main/java/walker/blue/demo/app/fragments/InitFragment.java
@@ -10,9 +10,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 import java.util.List;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import walker.blue.core.lib.init.InitializeProcess;
@@ -25,34 +26,40 @@ public class InitFragment extends Fragment {
     private static final int VOICE_PROMPT_REQUEST_CODE = 24;
     private static final String SPEECH_PROMPT = "Choose your destination";
 
-    private List<String> userInput;
-    private TextView textView;
+    private ExecutorService executorService;
+
     @SuppressLint("InflateParams")
     @Override
     public View onCreateView(final LayoutInflater inflater,
                              final ViewGroup container,
                              final Bundle savedInstanceState) {
-        textView = new TextView(getActivity());
-        InitializeProcess initializeProcess = new InitializeProcess(getActivity());
-        Executors.newSingleThreadExecutor().submit(initializeProcess);
-//        Toast.makeText(getActivity(), "Done Calling stuff", Toast.LENGTH_SHORT).show();
-        return textView;
+        if (executorService == null) {
+            this.executorService = Executors.newSingleThreadExecutor();
+        }
+        final Button button = new Button(this.getActivity());
+        button.setText("Initialize process");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getUserInput();
+            }
+        });
+
+        return button;
     }
 
     @Override
     public void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
-        Log.d(this.getClass().getName(), "onActivityResult - " + requestCode + " - " + resultCode);
         if (requestCode == VOICE_PROMPT_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-            this.userInput = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-            for (String s : this.userInput) {
-                Log.d(this.getClass().getName(), s);
+            final List<String> userInput = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            for (final String s : userInput) {
+                Log.d(this.getClass().getName(), String.format("\t%s", s));
             }
-            textView.setText(userInput.toString());
+            this.executorService.submit(new InitializeProcess(this.getActivity(), userInput));
         }
     }
 
     private void getUserInput() {
-        Log.d(this.getClass().getName(), "getUserInput");
         final Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT, SPEECH_PROMPT);
